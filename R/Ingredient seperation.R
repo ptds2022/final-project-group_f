78cbe33b48d1700c7ad4e9f4bea299c3da8c6165
joost0252 <33458844+joost0252@users.noreply.github.com>
2022-12-18 23:52:31 +0100

    Removed redundant code.

--- R/Ingredient seperation.R
+++ R/Ingredient seperation.R
@@ -20,19 +20,5 @@ library(stringr)
 RecipeNLG_dataset <- read_csv("data-raw/Recipe_new.csv")
 hunder_rows <- head(RecipeNLG_dataset, 100)
 
-# create a data frame with a column of strings with values between parentheses
-product_name <- hunder_rows["NER"]
-
-# extract the strings between parentheses using str_extract and a regular expression
-product_name <- product_name %>%
-  mutate(extracted_strings = apply(product_name, 1, function(x) str_extract(x, "\\(([^)]+)\\)")))
-
-# print the data frame
-print(product_name)
-
 ingredient_sep <- unlist(str_extract_all(hunder_rows$NER,  "\"([^\"]*)\""))
 ingredient_sep <- gsub("\"", "", ingredient_sep)
-
-#hunder_rows$NER <- unlist(hunder_rows$NER)
-
-#hunder_rows$NER <- lapply(hunder_rows$NER, unlist)
