9ed9fc5a0e8cd57b74663dee703657283d3cc8f5
joost0252 <33458844+joost0252@users.noreply.github.com>
2022-12-20 00:01:32 +0100

    Removed libraries and put them in Setup

--- R/Analysis recipe.R
+++ R/Analysis recipe.R
@@ -3,24 +3,10 @@
 ##packages
 pckg <- c('quanteda','ggwordcloud','quanteda.textplots','broom','tidytext','quanteda.textstats')
 install.packages(pckg)
+
 ## ---- warning=FALSE, message=FALSE----------------
 ## Libraries needed for the exercises
-library(readr)
-library(quanteda)
-library(quanteda.textplots)
-library(quanteda.textstats)
-library(tidytext)
-library(dplyr)
-library(ggplot2)
-library(broom)
-library(ggwordcloud)
-library(igraph)
-library(quanteda)
-library(quanteda.textstats)
-library(quanteda.textplots)
-library(stringr)
-
-RecipeNLG_dataset <- read_csv("data-raw/Recipe_new.csv")
+RecipeNLG_dataset <- read_csv("../data-raw/Recipe_new.csv")
 hunder_rows <- head(RecipeNLG_dataset, 1000)
 ingredient_sep <- unlist(str_extract_all(hunder_rows$NER,  "\"([^\"]*)\""))
 View(hunder_rows)
@@ -34,4 +20,5 @@ medium_difficulty <- mean(hunder_rows$lenghts_ingredient)
 max_difficulty <- max(hunder_rows$lenghts_ingredient)
 
 hunder_rows_analysis <- hunder_rows %>%
-  mutate(Difficulty = case_when( lenghts_ingredient > (medium_difficulty/2) & lenghts_ingredient <= (medium_difficulty*1.5) ~ "medium", lenghts_ingredient > (medium_difficulty*1.5) ~ "hard", TRUE ~ "easy"))
+  mutate(Difficulty = case_when( lenghts_ingredient > (medium_difficulty*0.8) & lenghts_ingredient <= (medium_difficulty*1.1) ~ "medium", lenghts_ingredient > (medium_difficulty*1.1) ~ "hard", TRUE ~ "easy"))
+
