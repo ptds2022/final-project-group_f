79efce3b762c7ee788063251b4d8f92109285ddc
Amina Mohammed <102906868+Vkmina@users.noreply.github.com>
2022-12-19 22:46:09 +0100

    Update

--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -1,8 +1,10 @@
 #' Recipe data from Kaggle
 #'
-#' @format ## Recipes
-#' A data frame with 2,231,142 rows and 6 ccolumns:
+#' @format ## RecipeNLG
+#' A raw data frame of 2,231,142 and nd 7 columns cooking recipes in RecipeNLG_dataset.csv (2.14 GB)
+#' A data frame with 2,231,142 rows and 7 columns. Subset 10,000 rows
 #' \describe{
+#'   \item{#}{Identification number of each row}
 #'   \item{title}{1312871 unique values}
 #'   \item{ingriedients}{2226362 unique values}
 #'   \item{directions}{2211644 unique values}
@@ -11,7 +13,7 @@
 #'   \item{NER}{2133496 unique values}
 #' }
 #' @source <https://www.kaggle.com/datasets/paultimothymooney/recipenlg?resource=download&select=RecipeNLG_dataset.csv>
-"recipes"
+"RecipeNLG (cooking recipes dataset)"
 
 
 usethis::use_data(DATASET, overwrite = TRUE)
