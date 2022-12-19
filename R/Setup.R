137cb98179e462df8cd895e0f8732e5c21811267
joost0252 <33458844+joost0252@users.noreply.github.com>
2022-12-19 23:21:20 +0100

    Added 1000 row creation.

--- R/Setup.R
+++ R/Setup.R
@@ -53,10 +53,33 @@ opts_chunk$set(
   echo = FALSE
 )
 
+##################### THis was previously the Ingredient seperation.R file ################################
+# install and load the stringr and dplyr packages if they're not already installed
+#install.packages(c("stringr", "dplyr"))
+library(stringr)
+library(dplyr)
+library(readr)
+library(quanteda)
+library(quanteda.textplots)
+library(quanteda.textstats)
+library(tidytext)
+library(dplyr)
+library(ggplot2)
+library(broom)
+library(ggwordcloud)
+library(igraph)
+library(quanteda)
+library(quanteda.textstats)
+library(quanteda.textplots)
+library(stringr)
+
+RecipeNLG_dataset <- read_csv("data-raw/Recipe_new.csv")
+hunder_rows <- head(RecipeNLG_dataset, 1000)
+
+ingredient_sep <- unlist(str_extract_all(hunder_rows$NER,  "\"([^\"]*)\""))
+ingredient_sep <- gsub("\"", "", ingredient_sep)
+
 ##################################################################################
 ## The following code creates the needed data in the environment to run the app ##
 ##################################################################################
-# These three lines give an error, so where to put them?
-#RecipeNLG_dataset <- read_csv("data-raw/Recipe_new.csv")
-#hunder_rows <- head(RecipeNLG_dataset, 100)
-#ingredient_sep <- unlist(str_extract_all(hunder_rows$NER,  "\"([^\"]*)\""))
+source("Ingredient_present.R")
