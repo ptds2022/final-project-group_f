ada35b2a11c2c6978625c66aace65dab231a343e
joost0252 <33458844+joost0252@users.noreply.github.com>
2022-12-19 15:56:10 +0100

    Added some extra columns, for now not useful, but I hope to use these in the future to finish the function.

--- R/Ingredient present.R
+++ R/Ingredient present.R
@@ -10,3 +10,11 @@ ingredients <- c("brown sugar", "nuts", "vanilla", "milk", "butter", "bite size 
 df$sub_ing <- map_chr(df$NER, ~ paste(str_extract(.x, ingredients), collapse = ", "))
 
 df$has_empty <- str_detect(df$sub_ing, "NA")
+
+df$na_count <- str_count(df$sub_ing, "NA")
+
+df$sub_ing_count <- str_count(df$sub_ing, ", ") + 1
+
+df$ingredient_count <- str_count(df$NER, ", ") + 1
+
+df$all_ingredients <- df$ingredient_count - (df$sub_ing_count - df$na_count)
