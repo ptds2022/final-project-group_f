c32303de35cc0e3d6bebcd51579db8ee6832a639
Amina Mohammed <102906868+Vkmina@users.noreply.github.com>
2022-12-20 03:08:39 +0100

    Uptade

--- TESTO/EDA recipe.R
+++ TESTO/EDA recipe.R
@@ -1,5 +1,5 @@
 
-source(here::here("R/Setup.R"))
+#source(here::here("R/Setup.R"))
 #source(here::here("data-raw/"))
 
 recipe_data <- read_csv("data-raw/Recipe_new.csv")
@@ -7,14 +7,13 @@ RecipeNLG_dataset <- read_csv("data-raw/Recipe_new.csv")
 
 hunder_rows <- head(RecipeNLG_dataset, 10000)
 sapply(recipe_data, class)
-
+n=3
 print(recipe_data["NER"][n,1])
 recipe_data["Ingriedients"] = as.list(recipe_data["NER"])
 
 #length(recipe_data_new["4","NER"][[1]])
 
 #Reach/print the n'th row
-n=3
 print(recipe_data["Ingriedients"][n,1])
 list = as.list.data.frame(recipe_data["Ingriedients"][n,1])
 
@@ -38,8 +37,6 @@ recipe.tokens = recipe.tokens[,!(names(recipe.tokens) %in% c("ingredients",  "di
 as.data.frame(recipe.tokens)
 
 require(reshape2)
-x <- dcast(recipe.tokens, title ~ X, value.var = paste0("word", 1:10))
-head(x)
 
 # Text Mining
 crude.cp <- corpus(hunder_rows$NER) #each row = text distinct
@@ -79,14 +76,3 @@ ggplot(data = crude.cos.mat, aes(x=X1, y=X2, fill=value)) +
     axis.text.y = element_text(size = 5)) +
   xlab("") +
   ylab("")
-
-### Add Difficulty columns
-hunder_rows$lenghts_ingredient <- lengths(gregexpr("\\W+", hunder_rows$NER)) + 1
-
-low_difficulty <- min(hunder_rows$lenghts_ingredient)
-medium_difficulty <- mean(hunder_rows$lenghts_ingredient)
-max_difficulty <- max(hunder_rows$lenghts_ingredient)
-
-hunder_rows_analysis <- hunder_rows %>%
-  mutate(Difficulty = case_when( lenghts_ingredient > (medium_difficulty/2) & lenghts_ingredient <= (medium_difficulty*1.5) ~ "medium", lenghts_ingredient > (medium_difficulty*1.5) ~ "hard", TRUE ~ "Low"))
-
